f9567f4193020f28688cf3f2195e405c99612fd5
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2024-04-04 16:14:14 +1100

    feat: add tests for min stack

--- src/test/java/org/example/MinStackTest.java
+++ src/test/java/org/example/MinStackTest.java
@@ -1,23 +1,40 @@
 package org.example;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MinStackTest {
+public class MinStackTest {
 
     @Test
-    @DisplayName("My first test")
-    void add() {
-
-        assertEquals(4, 2 + 2);
+    @DisplayName("Finds minimum when first element")
+    void findMinStart() {
+        MinStack ms = new MinStack();
+        ms.push(1);
+        ms.push(2);
+        ms.push(3);
+        assertEquals(ms.getMin(), 1);
     }
 
     @Test
-    @DisplayName("My second test")
-    void addTwo() {
+    @DisplayName("Finds minimum when middle element")
+    void findMinMiddle() {
+        MinStack ms = new MinStack();
+        ms.push(2);
+        ms.push(1);
+        ms.push(3);
+        assertEquals(ms.getMin(), 1);
+    }
 
-        assertEquals(4, 2 + 3);
+    @Test
+    @DisplayName("Finds minimum when last element")
+    void findMinEnd() {
+        MinStack ms = new MinStack();
+        ms.push(2);
+        ms.push(3);
+        ms.push(1);
+        assertEquals(ms.getMin(), 1);
     }
 }
