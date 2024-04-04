fde7e0944e6f5b4af4ce4f41088ae0891379da17
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2024-04-04 16:08:55 +1100

    feat: setup example test file

---
+++ src/test/java/org/example/MinStackTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MinStackTest {
+
+    @Test
+    @DisplayName("My first test")
+    void add() {
+
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    @DisplayName("My second test")
+    void addTwo() {
+
+        assertEquals(4, 2 + 3);
+    }
+}
